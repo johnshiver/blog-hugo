cf78ed667c261c24d6c188365708b9ea2991921f
John Shiver <johneshiver@gmail.com>
2019-04-29 12:16:58 -0400

    deploy script recurse submodules

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -15,7 +15,7 @@ rm -rf $BACKUP_WWW;
 cp -r $PUBLIC_WWW $BACKUP_WWW
 rm -rf $WORKING_DIRECTORY
 
-git clone $GIT_REPO $WORKING_DIRECTORY
+git clone --recurse-submodules $GIT_REPO $WORKING_DIRECTORY
 rm -rf $PUBLIC_WWW/
 /usr/local/bin/hugo -s $WORKING_DIRECTORY -d $PUBLIC_WWW -b "http://${MY_DOMAIN}"
 rm -rf $WORKING_DIRECTORY
